e36d939df664e2bce6f976415f39b4e618c319e9
Thomas Grindinger <tgrindinger@gmail.com>
2016-12-13 22:09:59 -0600

    remove utils test from naive bayes tests

--- testsuite/classifiers/NaiveBayesTest.hs
+++ testsuite/classifiers/NaiveBayesTest.hs
@@ -9,31 +9,7 @@ import Data.HashMap.Strict (HashMap, toList)
 import Test.QuickCheck
 
 testNaiveBayes = do
-  quickCheckResult prop_partitionData
   quickCheckResult prop_getMeanAndVariance
-
--- test partitionData
-newtype InputData = InputData { unInputData :: [[Double]] } deriving (Show, Eq)
-
-instance Arbitrary InputData where
-  arbitrary = do
-    size <- choose (1, 50)
-    newData <- listOf1 $ vectorOf size arbitrary
-    return $ InputData newData
-
-unpartitionData :: HashMap Double [[Double]] -> [[Double]]
-unpartitionData hashMap = concatMap (uncurry classedLists) $ toList hashMap
-  where classedLists classVal = fmap (classVal :)
-
-partitionData' :: InputData -> HashMap Double [[Double]]
-partitionData' = partitionData . unInputData
-
-prop_partitionData inputData = result
-  where undone    = unpartitionData $ partitionData' inputData
-        cond      = all (flip elem $ unInputData inputData) undone && length (unInputData inputData) == length undone
-        collected = collect (show $ 10 * mod (length $ unInputData inputData) 10) cond
-        countered = counterexample ("counter:\n" ++ show (partitionData' inputData) ++ "\n" ++ show (unInputData inputData) ++ "\n" ++ show undone) cond
-        result = if cond then collected else countered
 
 -- test getMeanAndVariance
 newtype StatData = StatData   { unStatData  ::  [Double]  } deriving (Show, Eq)
